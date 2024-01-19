7875985094fd5548fe4677bedb94ff6426d541ee
TaeYoungPar <tpark@oswego.edu>
2024-01-19 16:57:36 +0900

    Chore: 디자인ui경로설정

---
+++ apps/web/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@ui/Button';
+
+const page = () => {
+  return (
+    <div>
+      <Button>버튼이요</Button>
+    </div>
+  );
+};
+
+export default page;
