7cc7a211eead33746f82f420d895f5fd3782b5a1
seonnn <ssni0105@gmail.com>
2024-01-19 15:45:08 +0900

    Chore: sfac 프로젝트 font 설정

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,55 +1,54 @@
 import Test from '../components/Test';
 
 function Gradient({
-    conic,
-    className,
-    small,
+  conic,
+  className,
+  small,
 }: {
-    small?: boolean;
-    conic?: boolean;
-    className?: string;
+  small?: boolean;
+  conic?: boolean;
+  className?: string;
 }): JSX.Element {
-    return (
-        <span
-            className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
-                small ? 'blur-[32px]' : 'blur-[75px]'
-            } ${conic ? 'bg-glow-conic' : ''} ${className}`}
-        />
-    );
+  return (
+    <span
+      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
+        small ? 'blur-[32px]' : 'blur-[75px]'
+      } ${conic ? 'bg-glow-conic' : ''} ${className}`}
+    />
+  );
 }
 
 const LINKS = [
-    {
-        title: 'Docs',
-        href: 'https://turbo.build/repo/docs',
-        description:
-            'Find in-depth information about Turborepo features and API.',
-    },
-    {
-        title: 'Learn',
-        href: 'https://turbo.build/repo/docs/handbook',
-        description: 'Learn more about monorepos with our handbook.',
-    },
-    {
-        title: 'Templates',
-        href: 'https://turbo.build/repo/docs/getting-started/from-example',
-        description:
-            'Choose from over 15 examples and deploy with a single click.',
-    },
-    {
-        title: 'Deploy',
-        href: 'https://vercel.com/new',
-        description:
-            ' Instantly deploy your Turborepo to a shareable URL with Vercel.',
-    },
+  {
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
+  },
+  {
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
+  },
+  {
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.',
+  },
+  {
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
+    description:
+      ' Instantly deploy your Turborepo to a shareable URL with Vercel.',
+  },
 ];
 
 export default function Page(): JSX.Element {
-    return (
-        <main className='flex flex-col items-center justify-between min-h-screen p-24'>
-            <div>
-                <Test />
-            </div>
-        </main>
-    );
+  return (
+    <main className='flex flex-col items-center justify-between min-h-screen p-24'>
+      <div>
+        <Test />
+        <span>TEST test 123123 font 테스트를 해봅니다!!!</span>
+      </div>
+    </main>
+  );
 }
